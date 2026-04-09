2dbe57a332d165eb2920da565859ed55b8ba7ca3
Luis <seivaneluis@gmail.com>
2026-04-09 18:39:13 -0300

    Primer commit: proyecto HolaMundoMAUI inicial

---
+++ HelloMAUI/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Se cliequearon {count} veces";
+            else
+                CounterBtn.Text = $"Se cliequearon {count} veces";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
